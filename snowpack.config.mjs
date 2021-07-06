4c9490f9aaefe18ec49df33834c8cd9105a988f8
Jon Donais <jon.donais@animikii.com>
2021-07-06 12:13:11 -0400

    Emit single bundle on snowpack build.

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,11 +1,17 @@
 export default {
   mount: {
-   src: '/',
+    src: "/",
   },
   plugins: [
     '@snowpack/plugin-vue',
   ],
   buildOptions: {
     sourcemaps: true
+  },
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: 'es2018'
   }
+
 };
